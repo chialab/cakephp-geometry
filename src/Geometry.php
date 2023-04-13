e342947b519c5b7a3bbc934f11e4cd883f6bc2f1
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2023-04-13 16:59:09 +0200

    add serializeAs property

--- src/Geometry.php
+++ src/Geometry.php
@@ -11,6 +11,7 @@ use Brick\Geo\IO\EWKTReader;
 use Brick\Geo\IO\GeoJSONReader;
 use Brick\Geo\IO\GeoJSONWriter;
 use Brick\Geo\IO\WKBReader;
+use Brick\Geo\IO\WKBWriter;
 use Brick\Geo\IO\WKTWriter;
 use InvalidArgumentException;
 
@@ -23,6 +24,11 @@ class Geometry implements \JsonSerializable
      * The brick geometry instance.
      */
     protected BrickGeometry $geometry;
+
+    /**
+     * Serialization format.
+     */
+    protected static ?string $serializeAs;
 
     /**
      * Create a geometry wrapper.
@@ -96,11 +102,30 @@ class Geometry implements \JsonSerializable
     /**
      * Serialize the geometry object.
      *
-     * @return \stdClass The serialized geometry.
+     * @return mixed The serialized geometry.
      */
-    public function jsonSerialize(): \stdClass
+    public function jsonSerialize()
     {
-        return (new GeoJSONWriter())->writeRaw($this->geometry);
+        switch (static::$serializeAs) {
+            case 'wkb':
+                return (new WKBWriter())->write($this->geometry);
+
+            case 'wkt':
+                $locale = setlocale(LC_NUMERIC, 0);
+                setlocale(LC_NUMERIC, 'C');
+                try {
+                    $writer = new WKTWriter();
+                    $writer->setPrettyPrint(false);
+
+                    return $writer->write($this->geometry);
+                } finally {
+                    setlocale(LC_NUMERIC, $locale);
+                }
+
+            case 'geojson':
+            default:
+                return (new GeoJSONWriter())->writeRaw($this->geometry);
+        }
     }
 
     /**
@@ -124,5 +149,27 @@ class Geometry implements \JsonSerializable
     public function __clone()
     {
         $this->geometry = clone $this->geometry;
+    }
+
+    /**
+     * Set the serialization format.
+     * Supported formats are: wkb, wkt, geojson.
+     *
+     * @param string $serializeAs The serialization format.
+     * @return void
+     */
+    public static function setSerializeAs(string $serializeAs): void
+    {
+        switch ($serializeAs) {
+            case 'wkb':
+            case 'wkt':
+            case 'geojson':
+                static::$serializeAs = $serializeAs;
+
+                return;
+
+            default:
+                throw new InvalidArgumentException(sprintf('Invalid serialization format: %s', $serializeAs));
+        }
     }
 }
